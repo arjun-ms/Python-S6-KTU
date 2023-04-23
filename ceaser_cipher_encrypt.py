ba66958fdaff675f53e2447e265f035acb8d5b78
Arjun M S <arjunachu123.aa@gmail.com>
2023-04-23 08:21:15 +0530

    Initial commit

---
+++ ceaser_cipher_encrypt.py
@@ -0,0 +1,18 @@
+msg = input("enter a word: ")
+dist = int(input("enter a distance: "))
+code = ""
+
+
+for ch in msg:
+    # represents integer value of a letter
+    ordvalue = ord(ch)
+    ciphervalue = ordvalue + dist
+    
+    if ciphervalue > ord('z'):
+        # If the cipher value goes beyond 'z', wrap around to 'a'
+        # by subtracting the difference between the cipher value and 'z' 
+        # from the shift distance, and adding the result to the ASCII value of 'a'
+        ciphervalue = ord('a') + dist - (ord('z') - ordvalue + 1)
+    code += chr(ciphervalue)
+
+print(code)
